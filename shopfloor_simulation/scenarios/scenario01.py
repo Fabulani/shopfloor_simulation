2bb9125cc93090fa0cddb12369107a789ac30e33
Fabiano Manschein <fabianomanschein2@gmail.com>
2021-02-11 16:47:11 +0100

    Update to comply with new data model implemented in entities.py
    - Remove all "update_tcp()". Tool center point is no longer present
    in the new data model, as such this method has been deleted;
    - Update to all Robot instantiation to comply with the new data model;
    - Change anything that used "Station.pos" to a constant list value. There
    is an issue where using "Station.pos" returns "((x, y, z), ) instead of just
    (x, y, z), which crashes Robot.move(). As I haven't figured out what is
    happening, I've circumvented the issue by using constant values. With
    the new data model, these details will probably be changed again anyway.
    - Change MQTT Publisher and Subscriber so they are now an instance of
    the Shopfloor (the state machine). This allows them to be called from
    simulation_sm.py as "Scenario.publisher", allowing me to not have to
    create their instances inside simulation_sm.py.

--- shopfloor_simulation/scenarios/scenario01.py
+++ shopfloor_simulation/scenarios/scenario01.py
@@ -4,7 +4,8 @@ from random import randint
 from mpmath import *
 from math import sqrt
 from shopfloor_simulation.state_machine import State, StateMachine
-from shopfloor_simulation.entities import StaticRobot, MobileRobot, AGV, Station, Job, ShopfloorManager
+from shopfloor_simulation.entities import StationaryRobot, MobileRobot, AGV, Station, Job, ShopfloorManager
+from shopfloor_simulation.mqtt_utils import ShopfloorPublisher, ShopfloorSubscriber
 
 
 STATE_SLEEP = 2  # Amount of time to wait between states.
@@ -16,11 +17,11 @@ STATE_SLEEP = 2  # Amount of time to wait between states.
 # This means that OP10 can start without having to move Robots around from the previous State (Idle). This is not the case for other OPs.
 
 
-class ShopFloor(StateMachine):
+class Shopfloor(StateMachine):
     def __init__(self):
         # Initial state
         print("[STATE] Idle")
-        StateMachine.__init__(self, ShopFloor.idle)
+        StateMachine.__init__(self, Shopfloor.idle)
 
     def runAll(self):
         ''' (OVERRIDDEN) Print the current state before executing it. '''
@@ -37,7 +38,7 @@ class Idle(State):
         sleep(STATE_SLEEP)
 
     def next(self):
-        return ShopFloor.op10
+        return Shopfloor.op10
 
 
 class OP10(State):
@@ -47,8 +48,6 @@ class OP10(State):
         S1.status = 'busy'
         S2.status = 'busy'
         A1.status = 'busy'
-        S1.update_tcp()
-        S2.update_tcp()
 
         sleep(STATE_SLEEP)
         J1.complete_step(Station1.op)
@@ -56,7 +55,7 @@ class OP10(State):
         S2.reset()
 
     def next(self):
-        return ShopFloor.transition_to_op20
+        return Shopfloor.transition_to_op20
 
 
 class OP20(State):
@@ -65,32 +64,26 @@ class OP20(State):
         S4.status = 'busy'
         M1.status = 'busy'
         M2.status = 'busy'
-        S4.update_tcp()
-        M1.update_tcp()
-        M2.update_tcp()
 
         sleep(STATE_SLEEP)
         J1.complete_step(Station2.op)
         S4.reset()
 
     def next(self):
-        return ShopFloor.transition_to_op30
+        return Shopfloor.transition_to_op30
 
 
 class OP30(State):
     def run(self):
         J1.begin_step(Station3.op)
         S3.status = 'busy'
-        S3.update_tcp()
-        M1.update_tcp()
-        M2.update_tcp()
 
         sleep(STATE_SLEEP)
         J1.complete_step(Station3.op)
         S3.reset()
 
     def next(self):
-        return ShopFloor.transition_to_op40
+        return Shopfloor.transition_to_op40
 
 
 class OP40(State):
@@ -98,10 +91,6 @@ class OP40(State):
         J1.begin_step(Station4.op)
         S5.status = 'busy'
         S6.status = 'busy'
-        S5.update_tcp()
-        S6.update_tcp()
-        M1.update_tcp()
-        M2.update_tcp()
 
         sleep(STATE_SLEEP)
         J1.complete_step(Station4.op)
@@ -111,7 +100,7 @@ class OP40(State):
         M2.reset()
 
     def next(self):
-        return ShopFloor.transition_to_op50
+        return Shopfloor.transition_to_op50
 
 
 class OP50(State):
@@ -122,7 +111,7 @@ class OP50(State):
         J1.complete_step(Station5.op)
 
     def next(self):
-        return ShopFloor.transition_to_op60
+        return Shopfloor.transition_to_op60
 
 
 class OP60(State):
@@ -134,30 +123,30 @@ class OP60(State):
         J1.status = 'completed'
 
     def next(self):
-        return ShopFloor.transition_to_idle
+        return Shopfloor.transition_to_idle
 
 
 class TransitionToIdle(State):
     '''Transition State from OP60 to Idle'''
 
     def run(self):
-        for entity in ShopFloor.entities:
+        for entity in Shopfloor.entities:
             entity.reset()
         sleep(5)
 
     def next(self):
-        return ShopFloor.idle
+        return Shopfloor.idle
 
 
 class TransitionToOP20(State):
     '''Transition State from OP10 to OP20'''
 
     def run(self):
-        A1.move_robot(Station2.pos)
+        A1.move_robot((370, 530, 0))
         sleep(STATE_SLEEP)
 
     def next(self):
-        return ShopFloor.op20
+        return Shopfloor.op20
 
 
 class TransitionToOP30(State):
@@ -166,11 +155,11 @@ class TransitionToOP30(State):
     def run(self):
         M1.move_robot((600, 360, 0))
         M2.move_robot((600, 470, 0))
-        A1.move_robot(Station3.pos)
+        A1.move_robot((480, 400, 0))
         sleep(STATE_SLEEP)
 
     def next(self):
-        return ShopFloor.op30
+        return Shopfloor.op30
 
 
 class TransitionToOP40(State):
@@ -179,58 +168,73 @@ class TransitionToOP40(State):
     def run(self):
         M1.move_robot((375, 245, 0))
         M2.move_robot((375, 345, 0))
-        A1.move_robot(Station4.pos)
+        A1.move_robot((250, 300, 0))
         sleep(STATE_SLEEP)
 
     def next(self):
-        return ShopFloor.op40
+        return Shopfloor.op40
 
 
 class TransitionToOP50(State):
     '''Transition State from OP40 to OP50'''
 
     def run(self):
-        A1.move_robot(Station5.pos)
+        A1.move_robot((250, 200, 0))
         sleep(STATE_SLEEP)
 
     def next(self):
-        return ShopFloor.op50
+        return Shopfloor.op50
 
 
 class TransitionToOP60(State):
     '''Transition State from OP50 to OP60'''
 
     def run(self):
-        A1.move_robot(Station6.pos)
+        A1.move_robot((250, 100, 0))
         sleep(STATE_SLEEP)
 
     def next(self):
-        return ShopFloor.op60
+        return Shopfloor.op60
 
 
 ''' Entity and State Machine initialization. '''
 # Instantiate Stations
-Station1 = Station((650, 160, 0), 'OP10')
-Station2 = Station((370, 530, 0), 'OP20')
-Station3 = Station((480, 400, 0), 'OP30')
-Station4 = Station((250, 300, 0), 'OP40')
-Station5 = Station((250, 200, 0), 'OP50')
-Station6 = Station((250, 100, 0), 'OP60')
+Station1 = Station("Station-001", "Station1", "stations",
+                   "I'm Station 001!", (650, 160, 0), 'OP10')
+Station2 = Station("Station-002", "Station2", "stations",
+                   "I'm Station 002!", (370, 530, 0), 'OP20')
+Station3 = Station("Station-003", "Station3", "stations",
+                   "I'm Station 003!", (480, 400, 0), 'OP30')
+Station4 = Station("Station-004", "Station4", "stations",
+                   "I'm Station 004!", (250, 300, 0), 'OP40')
+Station5 = Station("Station-005", "Station5", "stations",
+                   "I'm Station 005!", (250, 200, 0), 'OP50')
+Station6 = Station("Station-006", "Station6", "stations",
+                   "I'm Station 006!", (250, 100, 0), 'OP60')
 
-# Instantiate Static Robots
-S1 = StaticRobot((615, 100, 0), 'idle', 'static', "S1", "Static-Robot-001")
-S2 = StaticRobot((690, 100, 0), 'idle', 'static', "S2", "Static-Robot-002")
-S3 = StaticRobot((685, 415, 0), 'idle', 'static', "S3", "Static-Robot-003")
-S4 = StaticRobot((490, 600, 0), 'idle', 'static', "S4", "Static-Robot-004")
-S5 = StaticRobot((490, 320, 0), 'idle', 'static', "S5", "Static-Robot-005")
-S6 = StaticRobot((490, 270, 0), 'idle', 'static', "S6", "Static-Robot-006")
+# Instantiate Stationary Robots
+S1 = StationaryRobot("Stationary-Robot-001", "S1", "robots", "I'm Stationary Robot 001!", "stationary",
+                     initial_position=[615, 100, 0], initial_orientation=[0, 0, 0, 0], current_station=Station1.header)
+S2 = StationaryRobot("Stationary-Robot-002", "S2", "robots", "I'm Stationary Robot 002!", "stationary",
+                     initial_position=[690, 100, 0], initial_orientation=[0, 0, 0, 0], current_station=Station1.header)
+S3 = StationaryRobot("Stationary-Robot-003", "S3", "robots", "I'm Stationary Robot 003!", "stationary",
+                     initial_position=[685, 415, 0], initial_orientation=[0, 0, 0, 0], current_station=Station3.header)
+S4 = StationaryRobot("Stationary-Robot-004", "S4", "robots", "I'm Stationary Robot 004!", "stationary",
+                     initial_position=[490, 600, 0], initial_orientation=[0, 0, 0, 0], current_station=Station2.header)
+S5 = StationaryRobot("Stationary-Robot-005", "S5", "robots", "I'm Stationary Robot 005!", "stationary",
+                     initial_position=[490, 320, 0], initial_orientation=[0, 0, 0, 0], current_station=Station4.header)
+S6 = StationaryRobot("Stationary-Robot-006", "S6", "robots", "I'm Stationary Robot 006!", "stationary",
+                     initial_position=[490, 270, 0], initial_orientation=[0, 0, 0, 0], current_station=Station4.header)
 
 # Instantiate Mobile Robots
-M1 = MobileRobot((450, 590, 0), 'idle', 'mobile', "M1", "Mobile-Robot-001")
-M2 = MobileRobot((450, 485, 0), 'idle', 'mobile', "M2", "Mobile-Robot-002")
+M1 = MobileRobot("Mobile-Robot-001", "M1", "robots", "I'm Mobile Robot 001!", "mobile",
+                 initial_position=[450, 590, 0], initial_orientation=[0, 0, 0, 0], current_station=Station3.header)
+M2 = MobileRobot("Mobile-Robot-002", "M2", "robots", "I'm Mobile Robot 002!", "mobile",
+                 initial_position=[450, 485, 0], initial_orientation=[0, 0, 0, 0], current_station=Station2.header)
 
-# Instantiate AGV
-A1 = AGV(Station1.pos, 'idle', "agv", "A1", "AGV-001")
+# Instantiate AGV  #! initial_position=Station1.pos results in A1.pose["position"]=([650, 160, 0],), no idea why
+A1 = AGV("AGV-001", "A1", "robots", "I'm AGV 001!", "agv",
+         initial_position=[650, 160, 0], initial_orientation=[0, 0, 0, 0], current_station=Station1.header)
 
 # Instantiate Jobs
 J1 = Job("Job-001", pending_steps=[Station1.op, Station2.op,
@@ -240,19 +244,27 @@ J1 = Job("Job-001", pending_steps=[Station1.op, Station2.op,
 Sm = ShopfloorManager()
 
 # List of entities that need to be reset
-ShopFloor.entities = [S1, S2, S3, S4, S5, S6, M1, M2, A1, J1]
+Shopfloor.entities = [S1, S2, S3, S4, S5, S6, M1, M2, A1, J1]
 
-# Static variable initialization (State registration):
-ShopFloor.idle = Idle()
-ShopFloor.op10 = OP10()
-ShopFloor.op20 = OP20()
-ShopFloor.op30 = OP30()
-ShopFloor.op40 = OP40()
-ShopFloor.op50 = OP50()
-ShopFloor.op60 = OP60()
-ShopFloor.transition_to_idle = TransitionToIdle()
-ShopFloor.transition_to_op20 = TransitionToOP20()
-ShopFloor.transition_to_op30 = TransitionToOP30()
-ShopFloor.transition_to_op40 = TransitionToOP40()
-ShopFloor.transition_to_op50 = TransitionToOP50()
-ShopFloor.transition_to_op60 = TransitionToOP60()
+# MQTT Publisher and Subscriber
+Shopfloor.publisher = ShopfloorPublisher(
+    name="MQTT-P",
+    publishing_entities=Shopfloor.entities)
+Shopfloor.subscriber = ShopfloorSubscriber(
+    name="MQTT-S",
+    subscribed_topics=["freeaim/echo/ShopfloorSubscriber"])
+
+# Stationary variable initialization (State registration):
+Shopfloor.idle = Idle()
+Shopfloor.op10 = OP10()
+Shopfloor.op20 = OP20()
+Shopfloor.op30 = OP30()
+Shopfloor.op40 = OP40()
+Shopfloor.op50 = OP50()
+Shopfloor.op60 = OP60()
+Shopfloor.transition_to_idle = TransitionToIdle()
+Shopfloor.transition_to_op20 = TransitionToOP20()
+Shopfloor.transition_to_op30 = TransitionToOP30()
+Shopfloor.transition_to_op40 = TransitionToOP40()
+Shopfloor.transition_to_op50 = TransitionToOP50()
+Shopfloor.transition_to_op60 = TransitionToOP60()
