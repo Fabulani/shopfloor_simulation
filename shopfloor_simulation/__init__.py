81ca435944b95fa54b4f8f1144cc3f55b3ef8d5a
Fabiano Manschein <fabianomanschein2@gmail.com>
2021-02-11 16:46:21 +0100

    Update with new class names.

--- shopfloor_simulation/__init__.py
+++ shopfloor_simulation/__init__.py
@@ -1,5 +1,5 @@
 from .version import __version__
-from .entities import Robot, StaticRobot, MobileRobot, AGV, Job, Station
+from .entities import Robot, StationaryRobot, MobileRobot, AGV, Job, Station
 from .mqtt_utils import MqttGeneric
 from .state_machine import StateMachine, State
 
@@ -7,7 +7,7 @@ from .state_machine import StateMachine, State
 # If somebody does "from shopfloor_simulation import *", this is what they will be able to access:
 __all__ = [
     'Robot',
-    'StaticRobot',
+    'StationaryRobot',
     'MobileRobot',
     'AGV',
     'Job',
