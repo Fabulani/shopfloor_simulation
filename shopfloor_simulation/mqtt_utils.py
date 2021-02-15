1d074897788d54079ed937cbea5efafbd44f1f27
Fabiano Manschein <fabianomanschein2@gmail.com>
2021-02-15 16:54:51 +0100

    Update to MQTT payload publishing and ShopfloorManager
    - All MQTT publishing is now handled by objects inside mqtt_utils.py;
    - ShopfloorPublisher now has a "send_payload" method, which
    gets an entity's attributes and turns it into a JSON payload. This payload
    is then sent to the entity's mqtt topic. With this, we are able to completely
    modularize the MQTT functionality: entites.py classes no longer require
    the mqtt_update_payload and mqtt_send_payload methods to work
    with MQTT, as well as no longer requiring any mqtt related attributes.
    Everything gets handled by the ShopfloorManager now;
    - ShopfloorPublisher's send_payload() method includes edge cases
    to avoid publishing repeated payloads;
    - Update MqttGeneric init function to include the root_topic attribute;
    - Removed the @staticmethod wrappers from the MQTT methods.
    They weren't necessary and, with their removal, it's now possible to
    call self;
    - Implement next_action method in ShopfloorManager for returning
    an action received via MQTT. These actions are now stored in a list
    object that works like a queue and are used by the StateMachine
    class in a scenario;
    - Add is_jsonable method to check if an object is JSON serializable.

--- shopfloor_simulation/mqtt_utils.py
+++ shopfloor_simulation/mqtt_utils.py
@@ -1,6 +1,7 @@
 import paho.mqtt.client as mqtt
 from time import sleep, perf_counter
 import json
+import copy
 
 
 ''' MQTT Connection setup. '''
@@ -9,13 +10,14 @@ MQTT_HOST = "mqtt.flespi.io"
 MQTT_PORT = 1883
 MQTT_USERNAME = ""
 MQTT_PASSWORD = ""
+ROOT_TOPIC = "freeaim/echo/"
 
 
 class MqttGeneric:
     '''Generic class for MQTT protocol communication'''
 
     def __init__(self, host=MQTT_HOST, port=MQTT_PORT, username=MQTT_USERNAME, password=MQTT_PASSWORD,
-                 run_event_check_sleep: float = 0.1, subscribed_topics: list = [], name="MQTT"):
+                 run_event_check_sleep: float = 0.1, subscribed_topics: list = [], name="MQTT", root_topic=ROOT_TOPIC):
         self.client = mqtt.Client()
         self.client.username_pw_set(username, password)
         self.client.on_connect = self.on_connect
@@ -25,6 +27,7 @@ class MqttGeneric:
         self.subscribed_topics = subscribed_topics
         self.run_event_check_sleep = run_event_check_sleep
         self.name = name
+        self.root_topic = root_topic
 
     def mqtt_loop(self, run_event):
         '''
@@ -34,14 +37,15 @@ class MqttGeneric:
             run_event = threading.Event()
             run_event.set()
 
-            mqtt_manager = MqttManager(MQTT_HOST, MQTT_PORT) 
-            mqtt_thread = threading.Thread(target=mqtt_manager.mqtt_loop, args=[run_event])
+            mqtt_manager = MqttManager(MQTT_HOST, MQTT_PORT)
+            mqtt_thread = threading.Thread(
+                target=mqtt_manager.mqtt_loop, args=[run_event])
             mqtt_thread.start()
 
             If the MQTT Manager will be used just for subscribing and receiving data, then this method will work fine.
             But if the MQTT Manager has to publish data, this method should be overridden to implement that.
 
-            `run_event`: event used to sync thread shutdown.    
+            `run_event`: event used to sync thread shutdown.
         '''
         self.client.loop_start()
         while run_event.is_set():
@@ -59,13 +63,11 @@ class MqttGeneric:
             client.subscribe(topic)
             print("[" + self.name + "] Subscribed to: " + topic)
 
-    @staticmethod
-    def on_message(client, userdata, msg):
+    def on_message(self, client, userdata, msg):
         '''The callback for when a PUBLISH message is received from the server.'''
         print("[" + self.name + "] " + msg.topic + " " + str(msg.payload))
 
-    @staticmethod
-    def on_publish(client, userdata, mid):
+    def on_publish(self, client, userdata, mid):
         '''The callback for when a message is published.'''
         print("[" + self.name + "] ({}s) (msgs={})".format(perf_counter(), mid))
 
@@ -73,26 +75,59 @@ class MqttGeneric:
 class ShopfloorPublisher(MqttGeneric):
     ''' MQTT Publisher that handles the Shopfloor's entities payloads. '''
 
-    def __init__(self, host=MQTT_HOST, port=MQTT_PORT, username=MQTT_USERNAME, password=MQTT_PASSWORD, run_event_check_sleep=0.1, publishing_entities=[], name='MQTT'):
+    def __init__(self, host=MQTT_HOST, port=MQTT_PORT, username=MQTT_USERNAME, password=MQTT_PASSWORD, run_event_check_sleep=0.1, publishing_entities=[], name='MQTT', root_topic=ROOT_TOPIC):
         super().__init__(host=host, port=port, username=username, password=password,
-                         run_event_check_sleep=run_event_check_sleep, subscribed_topics=[], name=name)
+                         run_event_check_sleep=run_event_check_sleep, subscribed_topics=[], name=name, root_topic=root_topic)
         self.publishing_entities = publishing_entities
+        self.prev_payloads = {}
 
     def mqtt_loop(self, run_event):
         '''(OVERRIDDEN) Starts the MQTT communication. Updates and sends payloads every loop.'''
-        # TODO: update and send payloads only when necessary
         self.client.loop_start()
+        sleep(1)
         while run_event.is_set():
             for entity in self.publishing_entities:
-                entity.mqtt_update_payload()
-                entity.mqtt_send_payload(self.client)
+                self.send_payload(entity)
                 sleep(0.01)
             sleep(self.run_event_check_sleep)
         self.client.loop_stop()
         print("[" + self.name + "] Shutting down.")
 
-    @staticmethod
-    def on_publish(client, userdata, mid):
+    def send_payload(self, entity):
+        '''Publish the entity's attributes as a JSON payload. If the payload is the same as the previous one, it will be ignored.'''
+
+        # Get the entity's attributes as a Python dict.
+        payload = vars(entity)
+
+        # Some attributes are objects, and those are not JSON serializable. Find them and get their vars()
+        for key in payload:
+            if not is_jsonable(payload[key]):
+                payload[key] = vars(payload[key])
+
+        # The payload id is used to verify if the new payload is different from its previous instance.
+        payload_id = entity.header["_id"]
+
+        # Verify if the payload exists in the prev_payload dict. If it does, check if the new and the prev are different.
+        if payload_id not in self.prev_payloads:
+            # Payload hasn't been registered yet. Register it.
+            self.prev_payloads[payload_id] = copy.deepcopy(payload)
+        else:
+            # A payload with the same id already exists. Check if they're different.
+            prev_payload = self.prev_payloads[payload_id]
+            if payload == prev_payload:
+                # The new payload is the same as its previous instance. Do nothing and return.
+                return
+
+        # Publish the new payload.
+        mqtt_topic = self.root_topic + \
+            entity.header["namespace"] + "/" + entity.header["_id"]
+        self.client.publish(
+            mqtt_topic, json.dumps(payload), 0)
+
+        # Update previous payload.
+        self.prev_payloads[payload_id] = copy.deepcopy(payload)
+
+    def on_publish(self, client, userdata, mid):
         '''(OVERRIDDEN) The callback for when a message is published. Do nothing.'''
         pass
 
@@ -107,3 +142,47 @@ class ShopfloorSubscriber(MqttGeneric):
             print("[" + self.name + "] Received: " + str(content))
         except:
             print("[" + self.name + "] Unrecognized: " + str(msg.payload))
+
+
+class ShopfloorManager(MqttGeneric):
+    ''' Receives commands from MQTT and influences the simulation accordingly. '''
+
+    def __init__(self, host=MQTT_HOST, port=MQTT_PORT, username=MQTT_USERNAME, password=MQTT_PASSWORD, run_event_check_sleep=0.1, subscribed_topics=[], name='MQTT', root_topic=ROOT_TOPIC):
+        super().__init__(host=host, port=port, username=username, password=password,
+                         run_event_check_sleep=run_event_check_sleep, subscribed_topics=subscribed_topics, name=name, root_topic=root_topic)
+        self.prev_action = ""
+        self.action_queue = []
+
+    def on_message(self, client, userdata, msg):
+        ''' (OVERRIDDEN) The callback for when a PUBLISH message is received from the server. '''
+        try:
+            content = json.loads(msg.payload.decode("utf-8"))
+            if "action" in content:
+                self.action_queue.append(content["action"])
+        except:
+            print("[" + self.name + "] Unrecognized: " + str(msg.payload))
+
+    def next_action(self):
+        ''' Return the next action from the action queue, but only if it's different from the previous action. '''
+        while len(self.action_queue) > 0:
+            # There are new actions in the queue.
+            next_action = self.action_queue[0]
+            if next_action != self.prev_action:
+                # New action received. Update prev_action and pop the new action from the queue.
+                self.prev_action = next_action
+                return self.action_queue.pop(0)
+            else:
+                # Repeated action received. Pop it from the list, but return an empty string.
+                self.action_queue.pop(0)
+        else:
+            # No new actions in the queue.
+            return ""
+
+
+def is_jsonable(x):
+    ''' Check if object is JSON serializable. '''
+    try:
+        json.dumps(x)
+        return True
+    except (TypeError, OverflowError):
+        return False
