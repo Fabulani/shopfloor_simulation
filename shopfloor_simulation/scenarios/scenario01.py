a6b250e792637764d61d29b9a63eb495e10eda4c
Fabiano Manschein <fabianomanschein2@gmail.com>
2021-02-23 19:19:39 +0100

    Add Job queue feature + Job manipulation via MQTT + new States
    - Add a function to handle State transitions according to Job status,
    and another to handle creating new Jobs and adding them to the queue
    - Update the current State prints to the terminal: now if the same State
    is repeated multiple times, instead of flooding the terminal, it'll be
    printed in the same line with the number of times it repeated besides it
    - Replace the action-based solution to Job manipulation with the Job
    Manager approach
    - Update to States to implement the new transitions according to Job
    status: only the States in the "IN_PROGRESS" section of the Job Flow
    (check the diagrams) received this update
    - Update Idle State: it'll loop forever on itself unless a message with
    "IN_PROGRESS" is sent to the Job's status topic.
    - Add a OP00 State
    - Add a FinishJob State: handles everything needed to be done before
    creating a new Job
    - Update Reset State: after resetting all entities in the resettable
    entities lists, it'll create a new Job
    - Update ProcessStep instantiation so it creates a deepcopy of the
    needed Operation instead of getting its reference: this is needed when
    creating new Jobs to avoid overwriting other Jobs PS or OPs
    - Replace the Shopfloor.entities list with the "resettable_entities" and
    "publishing_entities" list, since not all entities do both of resetting
    and publishing. Also, Jobs, PS and OPs won't be reset anymore, since
    Jobs will be created dynamically
    - Update the subscriber's topic to include the '+' wildcard, allowing it
    to watch all Job's statuses
    - Delete old unused imports

--- shopfloor_simulation/scenarios/scenario01.py
+++ shopfloor_simulation/scenarios/scenario01.py
@@ -1,20 +1,81 @@
-import json
+import copy
 from time import sleep
-from random import randint
-from mpmath import *
-from math import sqrt
 from shopfloor_simulation.state_machine import State, StateMachine
-from shopfloor_simulation.entities import StationaryRobot, MobileRobot, AGV, Station, Job, ProcessStep, Operation
-from shopfloor_simulation.mqtt_utils import ShopfloorPublisher, ShopfloorManager
+from shopfloor_simulation.entities import StationaryRobot, MobileRobot, Agv, Station, Job, ProcessStep, Operation
+from shopfloor_simulation.mqtt_utils import JobManager, ShopfloorPublisher
 
 
-STATE_SLEEP = 2  # Amount of time to wait between states.
+STATE_SLEEP = 0.5  # Amount of time to wait between states.
+
+
+''' Helper function definitions '''
+# These functions are specific to this scenario.
+
+
+def create_job():
+    ''' Create a new Job and add it to the Job Queue.
+
+        - The Job ID is determined by the lenght of the Job queue.
+        - The new Job does a deepcopy of the ProcessSteps to create entirely new
+        and independent Process Steps.
+        - The new Job is then appended to the appropriate lists and its topic
+        is initialized.
+    '''
+    ''' #! ISSUE
+    #! Deepcopy solves the issues of Jobs overwriting info on other Job objects,
+    #! but it doesn't solve the issue of overwriting the PS's and OP's
+    #! MQTT topics. Still, inside the Job topic, the PS and OP info should be OK.
+    '''
+    job_id = Shopfloor.job_count + 1
+    new_job = Job(
+        _id="JOB-" + str(job_id),
+        name="J" + str(job_id),
+        namespace="jobs",
+        description="I'm Job " + str(job_id) + "!",
+        process_steps=[
+            copy.deepcopy(Ps00),
+            copy.deepcopy(Ps01),
+            copy.deepcopy(Ps02),
+            copy.deepcopy(Ps03),
+            copy.deepcopy(Ps04),
+            copy.deepcopy(Ps05),
+            copy.deepcopy(Ps06)]
+    )
+    Shopfloor.job_queue.append(new_job)
+    Shopfloor.publishing_entities.append(new_job)
+    Shopfloor.publisher.initialize_single_topic(new_job)
+    Shopfloor.job_count += 1
+
+    for ps in new_job.process_steps:
+        for op in ps.operations:
+            Shopfloor.publishing_entities.append(op)
+            Shopfloor.publisher.initialize_single_topic(op)
+        Shopfloor.publishing_entities.append(ps)
+        Shopfloor.publisher.initialize_single_topic(ps)
+
+
+def check_job_status_then_change_state(current_state, next_state):
+    ''' Wrapper for the logic to check the Job status before state transition.
+
+        Since it's more straightfoward to have this check inside every State's
+        `next()` method (to avoid logic spaghetti in the StateMachine class),
+        this function will wrap the logic for easier maintenance.
+
+        Check the current Job status:
+        - If it's `IN_PROGRESS`, maintain normal state flow.
+        - If it's `ON_HOLD`, assign the current state to the prev_state variable
+        and then transition to the OnHold State.
+    '''
+    if Shopfloor.current_job.status == "IN_PROGRESS":
+        return next_state
+    elif Shopfloor.current_job.status == "ON_HOLD":
+        Shopfloor.prev_state = current_state
+        return Shopfloor.on_hold
 
 
 ''' State Machine and States setup. '''
-# The State Machine is a cycle: Idle -> OP10 -> TransitionToOP20 -> OP20 -> ... -> TransitionToIdle -> Idle
-# Note about TransitionToOP10: this State was discarded since all the robots are already positioned correctly in the TransitionToIdle State.
-# This means that OP10 can start without having to move Robots around from the previous State (Idle). This is not the case for other OPs.
+# The State Machine Diagram can be found here:
+# https://whimsical.com/shopfloor-simulation-state-machine-diagram-QUKKNykbMkyLasnx6ysvqB@2Ux7TurymLB2WJEDGpE5
 
 
 class Shopfloor(StateMachine):
@@ -25,39 +86,60 @@ class Shopfloor(StateMachine):
 
     def runAll(self):
         ''' (OVERRIDDEN) Print the current state before executing it. '''
+        prev_state_info = "[STATE] Idle"
+        state_info = ''
+        repeats = 0  # How many times has the same state_info been repeated
+
         while True:
-            action = Shopfloor.subscriber.next_action()
-            if action == "STOP":
-                self.previous_state = self.current_state
-                self.current_state = Shopfloor.stop
-            elif action == "CONTINUE":
-                self.current_state = self.previous_state.next()
+            # Update the Shopfloor Jobs' statuses
+            Shopfloor.subscriber.update_jobs(Shopfloor.job_queue)
+
+            # Transition to the next state
+            self.current_state = self.current_state.next()
+
+            # If the info is the same as before, print it on the same line.
+            state_info = "[STATE] " + self.current_state.__class__.__name__
+            if state_info != prev_state_info:
+                repeats = 0
+                print(state_info)
             else:
-                self.current_state = self.current_state.next()
-            print("[STATE] " + self.current_state.__class__.__name__)
+                repeats += 1
+                print(state_info + " x" + str(repeats), end='\r')
+
+            # Update prev_state_info
+            prev_state_info = state_info
+
+            # Run the current state
             self.current_state.run()
 
 
 class OnHold(State):
-    ''' Stop the simulation by doing nothing and loop on itself. 
+    ''' Stop the simulation by doing nothing and loop on itself.
 
-        Inside the Shopfloor's runAll method, this state can be triggered and changed:
-        - The "STOP" action will trigger this state;
-        - The "CONTINUE" action will trigger a state change.
+        This State will be triggered if the current Job has status `ON_HOLD`.
+        It'll go back to the previous state if the current Job status goes back
+        to `IN_PROGRESS`.
     '''
 
     def run(self):
         sleep(STATE_SLEEP)
 
     def next(self):
-        return Shopfloor.stop
+        if Shopfloor.current_job.status == "IN_PROGRESS":
+            return Shopfloor.prev_state
+        elif Shopfloor.current_job.status == "ON_HOLD":
+            return Shopfloor.on_hold
+        else:
+            # Keep looping on_hold if the status is not recognized.
+            # TODO: proper state for unknown status and other variants.
+            return Shopfloor.on_hold
 
 
 class Idle(State):
-    ''' Check the Job Queue and its Jobs. 
+    ''' Check the Job Queue and its Jobs.
 
     If there are Jobs and at least one of them has the `IN_PROGRESS` status,
-    transition to the `BeginJob` State after assigning the Job to 
+    transition to the `BeginJob` State after assigning the Job to
     `Shopfloor.current_job`. Otherwise, loop back to this state.
     '''
 
@@ -71,10 +153,10 @@ class Idle(State):
         sleep(STATE_SLEEP)
 
     def next(self):
-        if Shopfloor.current_job != None:
-            return Shopfloor.begin_job
-        else:
+        if Shopfloor.current_job == None:
             return Shopfloor.idle
+        else:
+            return Shopfloor.begin_job
 
 
 class BeginJob(State):
@@ -91,100 +173,131 @@ class BeginJob(State):
         sleep(STATE_SLEEP)
 
     def next(self):
-        return Shopfloor.op10
+        return check_job_status_then_change_state(Shopfloor.begin_job, Shopfloor.op00)
+
+
+class OP00(State):
+    def run(self):
+        Shopfloor.current_job.begin_process_step(0)
+
+        sleep(STATE_SLEEP)
+        Shopfloor.current_job.finish_process_step(0)
+
+    def next(self):
+        return check_job_status_then_change_state(Shopfloor.op00, Shopfloor.op10)
 
 
 class OP10(State):
     def run(self):
-        J1.begin_step(Station1.op)
-        J1.status = 'underway'
+        Shopfloor.current_job.begin_process_step(1)
+
         S1.status = 'busy'
         S2.status = 'busy'
         A1.status = 'busy'
 
         sleep(STATE_SLEEP)
-        J1.complete_step(Station1.op)
+        Shopfloor.current_job.finish_process_step(1)
         S1.reset()
         S2.reset()
 
     def next(self):
-        return Shopfloor.transition_to_op20
+        return check_job_status_then_change_state(Shopfloor.op10, Shopfloor.transition_to_op20)
 
 
 class OP20(State):
     def run(self):
-        J1.begin_step(Station2.op)
+        Shopfloor.current_job.begin_process_step(2)
         S4.status = 'busy'
         M1.status = 'busy'
         M2.status = 'busy'
 
         sleep(STATE_SLEEP)
-        J1.complete_step(Station2.op)
+        Shopfloor.current_job.finish_process_step(2)
         S4.reset()
 
     def next(self):
-        return Shopfloor.transition_to_op30
+        return check_job_status_then_change_state(Shopfloor.op20, Shopfloor.transition_to_op30)
 
 
 class OP30(State):
     def run(self):
-        J1.begin_step(Station3.op)
+        Shopfloor.current_job.begin_process_step(3)
         S3.status = 'busy'
 
         sleep(STATE_SLEEP)
-        J1.complete_step(Station3.op)
+        Shopfloor.current_job.finish_process_step(3)
         S3.reset()
 
     def next(self):
-        return Shopfloor.transition_to_op40
+        return check_job_status_then_change_state(Shopfloor.op30, Shopfloor.transition_to_op40)
 
 
 class OP40(State):
     def run(self):
-        J1.begin_step(Station4.op)
+        Shopfloor.current_job.begin_process_step(4)
         S5.status = 'busy'
         S6.status = 'busy'
 
         sleep(STATE_SLEEP)
-        J1.complete_step(Station4.op)
+        Shopfloor.current_job.finish_process_step(4)
         S5.reset()
         S6.reset()
         M1.reset()
         M2.reset()
 
     def next(self):
-        return Shopfloor.transition_to_op50
+        return check_job_status_then_change_state(Shopfloor.op40, Shopfloor.transition_to_op50)
 
 
 class OP50(State):
     def run(self):
-        J1.begin_step(Station5.op)
+        Shopfloor.current_job.begin_process_step(5)
 
         sleep(STATE_SLEEP)
-        J1.complete_step(Station5.op)
+        Shopfloor.current_job.finish_process_step(5)
 
     def next(self):
-        return Shopfloor.transition_to_op60
+        return check_job_status_then_change_state(Shopfloor.op50, Shopfloor.transition_to_op60)
 
 
 class OP60(State):
     def run(self):
-        J1.begin_step(Station6.op)
+        Shopfloor.current_job.begin_process_step(6)
 
         sleep(STATE_SLEEP)
-        J1.complete_step(Station6.op)
-        J1.status = 'completed'
+        Shopfloor.current_job.finish_process_step(6)
 
     def next(self):
-        return Shopfloor.transition_to_idle
+        return check_job_status_then_change_state(Shopfloor.op60, Shopfloor.finish_job)
 
 
-class TransitionToIdle(State):
-    '''Transition State from OP60 to Idle'''
+class FinishJob(State):
+    ''' Do anything needed to finish the Job. '''
 
     def run(self):
-        for entity in Shopfloor.entities:
+        Shopfloor.current_job.status = "DONE"
+        Shopfloor.job_queue.remove(Shopfloor.current_job)
+
+        # Remove PS and OPs from the publishing entities
+        for ps in Shopfloor.current_job.process_steps:
+            for op in ps.operations:
+                Shopfloor.publishing_entities.remove(op)
+            Shopfloor.publishing_entities.remove(ps)
+
+        Shopfloor.current_job = None
+        sleep(STATE_SLEEP)
+
+    def next(self):
+        return Shopfloor.reset
+
+
+class Reset(State):
+    '''Reset all Shopfloor resettable entities to their initial state.'''
+
+    def run(self):
+        for entity in Shopfloor.resettable_entities:
             entity.reset()
+        create_job()
         sleep(5)
 
     def next(self):
@@ -199,7 +312,7 @@ class TransitionToOP20(State):
         sleep(STATE_SLEEP)
 
     def next(self):
-        return Shopfloor.op20
+        return check_job_status_then_change_state(Shopfloor.transition_to_op20, Shopfloor.op20)
 
 
 class TransitionToOP30(State):
@@ -212,7 +325,7 @@ class TransitionToOP30(State):
         sleep(STATE_SLEEP)
 
     def next(self):
-        return Shopfloor.op30
+        return check_job_status_then_change_state(Shopfloor.transition_to_op30, Shopfloor.op30)
 
 
 class TransitionToOP40(State):
@@ -225,7 +338,7 @@ class TransitionToOP40(State):
         sleep(STATE_SLEEP)
 
     def next(self):
-        return Shopfloor.op40
+        return check_job_status_then_change_state(Shopfloor.transition_to_op40, Shopfloor.op40)
 
 
 class TransitionToOP50(State):
@@ -236,7 +349,7 @@ class TransitionToOP50(State):
         sleep(STATE_SLEEP)
 
     def next(self):
-        return Shopfloor.op50
+        return check_job_status_then_change_state(Shopfloor.transition_to_op50, Shopfloor.op50)
 
 
 class TransitionToOP60(State):
@@ -247,7 +360,7 @@ class TransitionToOP60(State):
         sleep(STATE_SLEEP)
 
     def next(self):
-        return Shopfloor.op60
+        return check_job_status_then_change_state(Shopfloor.transition_to_op60, Shopfloor.op60)
 
 
 ''' Entity and State Machine initialization. '''
@@ -260,40 +373,36 @@ Station5 = Station("Station-005", "Station5", "stations", "I'm Station 005!")
 Station6 = Station("Station-006", "Station6", "stations", "I'm Station 006!")
 
 # Instantiate Operations
-Op00 = Operation("OP-000", "Operation 00",
+Op00 = Operation("OP-000", "Op00",
                  "operations", "Main frame preparation")
-Op10 = Operation("OP-010", "Operation 10",
+Op10 = Operation("OP-010", "Op10",
                  "operations", "Sub-assemble cross member")
-Op20 = Operation("OP-020", "Operation 20",
+Op20 = Operation("OP-020", "Op20",
                  "operations", "Assemble cross member")
-Op30 = Operation("OP-030", "Operation 30",
+Op30 = Operation("OP-030", "Op30",
                  "operations", "Assemble rear member")
-Op40 = Operation("OP-040", "Operation 40",
+Op40 = Operation("OP-040", "Op40",
                  "operations", "Assemble front member")
-Op50 = Operation("OP-050", "Operation 50",
+Op50 = Operation("OP-050", "Op50",
                  "operations", "Measurement")
-Op60 = Operation("OP-060", "Operation 60",
+Op60 = Operation("OP-060", "Op60",
                  "operations", "Disassembly")
 
 # Instantiate Process Steps
-Ps00 = ProcessStep("PS-000", "Process Step 00", "process_steps",
-                   "I'm Process Step 00!", [Op00], Station1.header)
-Ps01 = ProcessStep("PS-001", "Process Step 01", "process_steps",
-                   "I'm Process Step 01!", [Op10], Station1.header)
-Ps02 = ProcessStep("PS-002", "Process Step 02", "process_steps",
-                   "I'm Process Step 02!", [Op20], Station2.header)
-Ps03 = ProcessStep("PS-003", "Process Step 03", "process_steps",
-                   "I'm Process Step 03!", [Op30], Station3.header)
-Ps04 = ProcessStep("PS-004", "Process Step 04", "process_steps",
-                   "I'm Process Step 04!", [Op40], Station4.header)
-Ps05 = ProcessStep("PS-005", "Process Step 05", "process_steps",
-                   "I'm Process Step 05!", [Op50], Station5.header)
-Ps06 = ProcessStep("PS-006", "Process Step 06", "process_steps",
-                   "I'm Process Step 06!", [Op60], Station6.header)
-
-# Instantiate Jobs
-J1 = Job("JOB-001", "Job 001", "jobs", "I'm Job 001!",
-         [Ps00, Ps01, Ps02, Ps03, Ps04, Ps05, Ps06])
+Ps00 = ProcessStep("PS-000", "Ps00", "process_steps", "I'm Process Step 00!",
+                   [copy.deepcopy(Op00)], Station1.header)
+Ps01 = ProcessStep("PS-001", "Ps01", "process_steps", "I'm Process Step 01!",
+                   [copy.deepcopy(Op10)], Station1.header)
+Ps02 = ProcessStep("PS-002", "Ps02", "process_steps", "I'm Process Step 02!",
+                   [copy.deepcopy(Op20)], Station2.header)
+Ps03 = ProcessStep("PS-003", "Ps03", "process_steps", "I'm Process Step 03!",
+                   [copy.deepcopy(Op30)], Station3.header)
+Ps04 = ProcessStep("PS-004", "Ps04", "process_steps", "I'm Process Step 04!",
+                   [copy.deepcopy(Op40)], Station4.header)
+Ps05 = ProcessStep("PS-005", "Ps05", "process_steps", "I'm Process Step 05!",
+                   [copy.deepcopy(Op50)], Station5.header)
+Ps06 = ProcessStep("PS-006", "Ps06", "process_steps", "I'm Process Step 06!",
+                   [copy.deepcopy(Op60)], Station6.header)
 
 # Instantiate Stationary Robots
 S1 = StationaryRobot("Stationary-Robot-001", "S1", "robots", "I'm Stationary Robot 001!", "stationary",
@@ -315,44 +424,53 @@ M1 = MobileRobot("Mobile-Robot-001", "M1", "robots", "I'm Mobile Robot 001!", "m
 M2 = MobileRobot("Mobile-Robot-002", "M2", "robots", "I'm Mobile Robot 002!", "mobile",
                  initial_position=[450, 485, 0], initial_orientation=[0, 0, 0, 0], current_station=Station2.header)
 
-# Instantiate AGV  #! initial_position=Station1.pos results in A1.pose["position"]=([650, 160, 0],), no idea why
-A1 = AGV("AGV-001", "A1", "robots", "I'm AGV 001!", "agv",
+# Instantiate AGV
+A1 = Agv("AGV-001", "A1", "robots", "I'm AGV 001!", "agv",
          initial_position=(650, 160, 0), initial_orientation=[0, 0, 0, 0], current_station=Station1.header)
 
-# List of entities
-Shopfloor.entities = [Station1, Station2, Station3, Station4, Station5, Station6,
-                      Op00, Op10, Op20, Op30, Op40, Op50, Op60,
-                      Ps00, Ps01, Ps02, Ps03, Ps04, Ps05, Ps06,
-                      J1,
-                      S1, S2, S3, S4, S5, S6,
-                      M1, M2,
-                      A1,
-                      ]
+# List of entities that have the reset() method
+Shopfloor.resettable_entities = [S1, S2, S3, S4, S5, S6,
+                                 M1, M2,
+                                 A1,
+                                 ]
 
-# Job management related variables
-Shopfloor.job_queue = [J1]
+# List of entities that publish data to MQTT
+Shopfloor.publishing_entities = [Station1, Station2, Station3, Station4, Station5, Station6,
+                                 S1, S2, S3, S4, S5, S6,
+                                 M1, M2,
+                                 A1,
+                                 ]
+
+# Initialize Job management related variables
+Shopfloor.job_queue = []
+Shopfloor.job_count = 0  # How many Jobs have been created
 Shopfloor.current_job = None  # Will store ref to Job objects
-
+Shopfloor.prev_state = None  # Will store a ref to the previous State
 
 # MQTT Publisher and Subscriber
 Shopfloor.publisher = ShopfloorPublisher(
     name="MQTT-P",
-    publishing_entities=Shopfloor.entities)
-Shopfloor.subscriber = ShopfloorManager(
+    publishing_entities=Shopfloor.publishing_entities)
+Shopfloor.subscriber = JobManager(
     name="MQTT-S",
-    subscribed_topics=["freeaim/echo/ShopfloorManager"])
+    subscribed_topics=["freeaim/echo/jobs/+/status"])
+
+# Create the first Job
+create_job()
 
 # Stationary variable initialization (State registration):
 Shopfloor.on_hold = OnHold()
 Shopfloor.idle = Idle()
 Shopfloor.begin_job = BeginJob()
+Shopfloor.finish_job = FinishJob()
+Shopfloor.op00 = OP00()
 Shopfloor.op10 = OP10()
 Shopfloor.op20 = OP20()
 Shopfloor.op30 = OP30()
 Shopfloor.op40 = OP40()
 Shopfloor.op50 = OP50()
 Shopfloor.op60 = OP60()
-Shopfloor.transition_to_idle = TransitionToIdle()
+Shopfloor.reset = Reset()
 Shopfloor.transition_to_op20 = TransitionToOP20()
 Shopfloor.transition_to_op30 = TransitionToOP30()
 Shopfloor.transition_to_op40 = TransitionToOP40()
